714225f173cbf5ec0410f54c93ce96f06dc6dcd6
Kay Siver Bø <578167@stud.hvl.no>
2021-11-10 08:30:49 +0100

    Network server port update

--- SemesterProject.NetworkCommunication/SocketServer.cs
+++ SemesterProject.NetworkCommunication/SocketServer.cs
@@ -91,7 +91,7 @@ namespace SemesterProject.NetworkCommunication
 			broadcastEndPoint = new IPEndPoint(IPAddress.Broadcast, 9001);
 			broadcastCanceller = new CancellationTokenSource();
 
-			broadcast = new UdpClient(6900);
+			broadcast = new UdpClient(42069);
 			broadcast.Connect(broadcastEndPoint);
 
 			Log.Information("Starting broadcaster on {0}", broadcast.Client.LocalEndPoint);
